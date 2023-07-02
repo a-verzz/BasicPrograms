138bfba90ae7c162a357e4fc09f39b15e52c8144
a-verzz <ansherzz@gmail.com>
2023-07-02 23:03:04 +0530

    changed variables.js

--- js/variables.js
+++ js/variables.js
@@ -13,3 +13,22 @@ console.log("message: "+messages);
 
 message="new Hello";
 console.log("new message: "+message);
+
+messages=message;
+console.log("new messages: "+messages);
+//one variable can be copied to other variables
+
+//a variable name can not start with a number
+
+// $ and _ can be used in naming a variable
+
+// hyphen is not allowed in varible names
+
+// without "use strict" mode, variables can be defined without let or var, as done in older versions of javascript. This method is not recommended
+
+const FIX=007;
+
+// this constant value will never change
+
+// constants are generally in uppercase
+
