cf48492d47b25b67c570e609d172f7e0063efa38
a-verzz <ansherzz@gmail.com>
2023-07-02 21:27:15 +0530

    variables in js

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+let message;
+message="hello"; // stores the string 'hello' in variable 'message'
+alert(message); // will show variable content;
+
+let user='John', age='23', messages='what\'s up';
+console.log("user"+user);
+console.log("age"+age);
+console.log("message"+messages);
