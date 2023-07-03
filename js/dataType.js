24b5ee05152562371e7b06c3ec3374addfeaee9a
a-verzz <ansherzz@gmail.com>
2023-07-03 23:58:44 +0530

    added js file

---
+++ js/dataType.js
@@ -0,0 +1,44 @@
+
+let message="hello";
+message=1234;
+
+let n=20;
+n=9.04;
+
+console.log(1/0);
+console.log("Infinity");
+console.log("no a number"/2);
+console.log(NaN+1);
+console.log(3*NaN);
+console.log("not a number"/2-1);
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+alert(`the result is ${1+2}`);
+
+let nameFieldChecked= true;
+
+let isGreater= 4>1;
+console.log("isGreater: "+isGreater);
+
+let age= null;
+
+let app;
+console.log("app: "+app);
+
+let ages=100;
+age=undefined;
+console.log("ages: "+ages);
+
+console.log(
+    "undefined: "+typeof undefined);
+console.log(
+    "number: "+ typeof 0);
+    
+console.log(
+    "bigInt: "+ typeof 10n
+);
+
+
