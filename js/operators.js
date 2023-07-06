92e52a1b9c4167c493c3de27754c0a86b8af96d6
a-verzz <ansherzz@gmail.com>
2023-07-06 23:40:12 +0530

    added operators examples

---
+++ js/operators.js
@@ -0,0 +1,25 @@
+let x = 1;
+
+x = -x;
+console.log( x ); // -1, unary negation was applied
+
+
+console.log(2 + 2 + '1' ); // "41" and not "221"
+
+let y=3;
+console.log( y - x ); // 4, binary minus subtracts values
+
+console.log( 5 % 2 ); // 1, the remainder of 5 divided by 2
+
+console.log( 2 ** 3 ); // 2³ = 8
+
+console.log( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
+
+let s = "my" + "string";// concatination with binary +
+console.log(s); // mystring 
+
+console.log( '1' + 2 ); // "12"
+
+console.log(2 + 2 + '1' ); // "41" and not "221"
+
+console.log('2' + 2 + '1' ); 
