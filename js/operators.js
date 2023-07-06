04c3bbe223211290750a40cd8408262e4321d5c5
a-verzz <ansherzz@gmail.com>
2023-07-06 23:48:43 +0530

    added some more examples in operators.js

--- js/operators.js
+++ js/operators.js
@@ -3,9 +3,6 @@ let x = 1;
 x = -x;
 console.log( x ); // -1, unary negation was applied
 
-
-console.log(2 + 2 + '1' ); // "41" and not "221"
-
 let y=3;
 console.log( y - x ); // 4, binary minus subtracts values
 
@@ -23,3 +20,17 @@ console.log( '1' + 2 ); // "12"
 console.log(2 + 2 + '1' ); // "41" and not "221"
 
 console.log('2' + 2 + '1' ); 
+
+console.log( 6 - '2' ); // 4, converts '2' to a number
+console.log( '6' / '2' ); // 3, converts both operands to numbers
+
+// No effect on numbers
+let a = 1;
+alert( +a ); // 1
+
+let by = -2;
+alert( +b ); // -2
+
+// Converts non-numbers
+alert( +true ); // 1
+alert( +"" );   // 0
